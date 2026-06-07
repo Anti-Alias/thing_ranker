c1e094b99ff74b85ec457207dabe63114836f89c
William Andrew Cahill <drewlikesramen@gmail.com>
2026-06-07 11:44:51 -0400

    Created hello-world REST api

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use axum::Router;
+use axum::routing::get;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/health", get(health));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health() -> &'static str {
+    "Hello, world"
+}
