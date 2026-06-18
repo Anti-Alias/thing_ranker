8adfdffce7a0ce1e72d94db07ea3c22b699cb7c0
William Andrew Cahill <drewlikesramen@gmail.com>
2026-06-17 21:01:37 -0400

    Added home and signin routes

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+  {
+    path: "/signin",
+    Component: SignIn,
+  }
+]);
+
+const root = document.getElementById("root")!;
+
+createRoot(root).render(
+  <RouterProvider router={router} />,
+);
